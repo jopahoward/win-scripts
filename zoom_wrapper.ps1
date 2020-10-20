50d2b1030c00be20f0e17d78b4485a0973c362fc
Joseph Howard <jphow@umich.edu>
2020-10-20 09:31:16 -0400

    Make zoom wrapper end zoom meetings, too

--- zoom_wrapper.ps1
+++ zoom_wrapper.ps1
@@ -1,8 +1,9 @@
 $ZOOM = Get-Process zoom -ErrorAction SilentlyContinue
 if (!${ZOOM}) {
-    $ZOOM = Start-Process ${Env:appdata}\Zoom\bin\zoom.exe -passthru
+    Start-Process ${Env:appdata}\Zoom\bin\zoom.exe -passthru
 }
 # Write-Host "${ZOOM_ID}"
 Write-Host "Press any key to end..."
 $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+$ZOOM = Get-Process zoom -ErrorAction SilentlyContinue
 ${ZOOM} |Stop-Process
